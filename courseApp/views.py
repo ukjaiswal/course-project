6199d877565ed74cd3a41e70c5f3ecb10b981138
Uttam Jaiswal <uttam@kavach.net>
2021-06-02 13:06:57 +0530

    Implimented Mixin

--- courseApp/views.py
+++ courseApp/views.py
@@ -1,12 +1,37 @@
 from django.shortcuts import render
 from .serializers import CourseSerializer
 from .models import Course
-from rest_framework import status
+from rest_framework import status, mixins, generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.http import Http404
 
 # Create your views here.
+class CourseList(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+    def get(self,request):
+        return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+
+class CourseDetail(mixins.RetrieveModelMixin,mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+    def get(self,request,pk):
+        return self.retrieve(request, pk)
+
+    def put(self, request, pk):
+        return self.update(request, pk)
+
+    def delete(self, request, pk):
+        return self.destroy(request, pk)
+
+"""
 class CourseList(APIView):
     def get(self,request):
         course = Course.objects.all()
@@ -46,3 +71,4 @@ class CourseDetail(APIView):
         course = self.get_object(pk)
         course.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+"""
